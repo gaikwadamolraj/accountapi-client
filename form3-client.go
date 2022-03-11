d7f8217b47bb68fcdb4920a4627b4882ddb000d1
Amol Gaikwad <amol_gaikwad@apple.com>
2022-03-11 18:16:46 +0000

    Error handle

--- form3-client.go
+++ form3-client.go
@@ -23,18 +23,32 @@ func main() {
 	accountData.SetAccountID(accountId)
 
 	log.Println("---------------- create --------------------")
-	req, _ := client.Create(ctx, accountData)
-	response, _ := client.Do(req)
+	req, err := client.Create(ctx, accountData)
+	if err == nil {
+		log.Println("Error occured")
+	}
+	response, err := client.Do(req)
+	if err != nil {
+		log.Println("Error occured")
+	}
 	if response.StatusCode == http.StatusCreated {
 		log.Printf("Acoounts Fetched sucessfully %s", response.Status)
 	} else {
 		log.Println(fmt.Sprintf("Error occured status code %s", response.Status))
 	}
+
 	log.Println("---------------- create --------------------")
 
 	log.Println("---------------- Fetch ---------------------")
-	req, _ = client.Fetch(ctx, accountId)
-	response, _ = client.Do(req)
+	req, err = client.Fetch(ctx, accountId)
+	if err != nil {
+		log.Println("Error occured")
+	}
+
+	response, err = client.Do(req)
+	if err != nil {
+		log.Println("Error occured")
+	}
 	if response.StatusCode == http.StatusOK {
 		log.Printf("Acoounts Fetched sucessfully %s", response.Status)
 	} else {
@@ -43,8 +57,16 @@ func main() {
 	log.Println("---------------- Fetch ---------------------")
 
 	log.Println("---------------- Delete --------------------")
-	req, _ = client.Delete(ctx, accountId, version)
-	response, _ = client.Do(req)
+	req, err = client.Delete(ctx, accountId, version)
+	if err != nil {
+		log.Println("Error occured")
+	}
+
+	response, err = client.Do(req)
+	if err != nil {
+		log.Println("Error occured")
+	}
+
 	if response.StatusCode == http.StatusNoContent {
 		log.Printf("Account deleted %s", response.Status)
 	} else {
